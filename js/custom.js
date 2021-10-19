436138f1f2b54cdc9673bc71da4f5ed09cc644c4
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-10-19 22:00:20 +0900

    count down ready

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+// get elements froom html
+var year = document.getElementById('year'); 
+
+var days = document.getElementById('days'); 
+var hours = document.getElementById('hours'); 
+var minutes = document.getElementById('minutes'); 
+var seconds = document.getElementById('seconds'); 
+
+var currentYear = new Date().getFullYear();
+
+var nextYear = new Date(`may 08 ${currentYear + 1} 00:00:00`);
+
+
+function countdown()
+{
+    var currentTime = new Date();
+    var diff = nextYear - currentTime;
+    var s = Math.floor(diff/1000) % 60;
+    var m = Math.floor(diff/1000/60) % 60; 
+    var h = Math.floor(diff/1000/60/60) % 24;
+    var d = Math.floor(diff/1000/60/60/24);
+    seconds.innerHTML = s;
+    minutes.innerHTML = m;
+    hours.innerHTML = h;
+    days.innerHTML = d;
+
+    
+
+}
+setInterval(countdown,1000);
+
+year.innerHTML = nextYear.getFullYear();
